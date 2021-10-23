36e214ac609528089edc3c1b47cf71ccbed0c62b
Caio Levy <caioley303@gmail.com>
2021-10-23 01:49:55 -0300

    Aula 05

---
+++ crud-angular/src/app/cursos/cursos/cursos.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './cursos.component.html',
+  styleUrls: ['./cursos.component.scss']
+})
+export class CursosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
